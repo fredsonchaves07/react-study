12b7f3e6f5201a290b1d26da1544dd0a382a617b
fredsonchaves07 <fredson.ribeiro@objective.com.br>
2022-07-14 21:47:30 -0300

    feat: adicionando estilizacao nos componentes de header

--- 01-fundamentos-react/src/App.jsx
+++ 01-fundamentos-react/src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
+
+import './global.css'
 
 export function App() {
   return (
     <div>
+        <Header />
         <Post author="Fredson Chaves" content="loren"/>
         <Post author="Ana Luiza" content="Novo post"/>
     </div>
