aa3153ce72cdf40936cdc728c8a829150ba5b74b
fredsonchaves07 <fredson.ribeiro@objective.com.br>
2022-07-29 13:40:32 -0300

    feat: criacao do componente avatar e estilizacao

--- 01-fundamentos-react/src/components/Sidebar.jsx
+++ 01-fundamentos-react/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 
 import styles from './Sidebar.module.css'
 
@@ -10,10 +11,7 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
             />
             <div className={styles.profile}>
-                <img 
-                    className={styles.avatar}
-                    src="https://github.com/fredsonchaves07.png"
-                />
+                <Avatar src="https://github.com/fredsonchaves07.png"/>
                 <strong>Fredson Chaves</strong>
                 <span>Web Developer</span>
             </div>
