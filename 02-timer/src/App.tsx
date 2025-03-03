7080ae709b05067b50fc461c9e30f1ad823b4ec8
fredsonchaves07 <fredson.ribeiro@objective.com.br>
2025-03-03 12:10:17 -0300

    feat: configurando projeto ignite timer

---
+++ 02-timer/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/Button";
+
+export function App() {
+  return (
+    <>
+      <Button color="primary"></Button>
+      <Button color="secondary"></Button>
+      <Button color="success"></Button>
+      <Button color="danger"></Button>
+      <Button></Button>
+    </>
+  )
+}
