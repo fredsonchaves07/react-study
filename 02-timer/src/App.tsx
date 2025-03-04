89cbff12f1a80484ad8f50285bbd4b08fef06b4f
fredsonchaves07 <fredson.ribeiro@objective.com.br>
2025-03-04 11:38:39 -0300

    feat: ajustando cores e estilizações globais com styled components

--- 02-timer/src/App.tsx
+++ 02-timer/src/App.tsx
@@ -1,13 +1,17 @@
+import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <>
-      <Button color="primary"></Button>
-      <Button color="secondary"></Button>
-      <Button color="success"></Button>
-      <Button color="danger"></Button>
-      <Button></Button>
-    </>
+      <ThemeProvider theme={defaultTheme}>
+        <Button color="primary"></Button>
+        <Button color="secondary"></Button>
+        <Button color="success"></Button>
+        <Button color="danger"></Button>
+        <Button></Button>
+        <GlobalStyle></GlobalStyle>
+    </ThemeProvider>
   )
 }
--- 02-timer/src/components/Button.styles.ts
+++ 02-timer/src/components/Button.styles.ts
@@ -15,9 +15,13 @@ const buttonVariants = {
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;
-    height: 40p;
+    height: 40px;
+    border-radius: 4px;
+    border: 0;
+    margin: 8px;
+    background-color: ${props => props.theme['green-500']};
 
-    ${props => {
+    /* ${props => {
         return css`background-color: ${buttonVariants[props.variant]}`
-    }}
+    }} */
 `
