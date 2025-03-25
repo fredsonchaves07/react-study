48f23a240c9b5b983fd413ce6328f3a8be174017
Fredson Chaves <fredsonchaves07@gmail.com>
2025-03-25 08:31:54 -0300

    feat: entendendo contextos no react

--- 02-timer/src/App.tsx
+++ 02-timer/src/App.tsx
@@ -1,16 +1,18 @@
-import { ThemeProvider } from 'styled-components'
-import { BrowserRouter } from 'react-router-dom'
-import { defaultTheme } from './styles/themes/default'
-import { GlobalStyle } from './styles/global'
-import { Router } from './Router'
+// import { ThemeProvider } from 'styled-components'
+// import { BrowserRouter } from 'react-router-dom'
+// import { defaultTheme } from './styles/themes/default'
+// import { GlobalStyle } from './styles/global'
+// import { Router } from './Router'
+import { Home } from './Home'
 
 export function App() {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-      <GlobalStyle></GlobalStyle>
-    </ThemeProvider>
-  )
+  // return (
+  //   <ThemeProvider theme={defaultTheme}>
+  //     <BrowserRouter>
+  //       <Router />
+  //     </BrowserRouter>
+  //     <GlobalStyle></GlobalStyle>
+  //   </ThemeProvider>
+  // )
+  return <Home />
 }
