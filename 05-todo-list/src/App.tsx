f34ede79384335c6d40eb919eb7222250514a5f1
Fredson Chaves <fredsonchaves07@gmail.com>
2025-06-05 11:26:01 -0300

    feat: finalizando ultimos componenentes

--- 05-todo-list/src/App.tsx
+++ 05-todo-list/src/App.tsx
@@ -8,13 +8,21 @@ import XIcon from "./assets/icons/X-Regular.svg?react"
 
 import Icon from "./components/icon";
 import Badge from "./components/badge";
+import Button from "./components/button";
+import ButtonIcon from "./components/button-icon";
+import InputText from "./components/input-text";
+import InputChecbox from "./components/input-checkbox";
+import Card from "./components/card";
+import Container from "./components/container";
+import Skeleton from "./components/skeleton";
 
 export default function App() {
   return (
-    <div className="grid gap-3">
+    <Container>
+    <div className="grid gap-10">
       <div className="flex flex-col gap-1">
         <Text variant={"body-md-bold"}>
-            Lear o dog para passear
+            Levar o dog para passear
         </Text>
       </div>
 
@@ -31,6 +39,29 @@ export default function App() {
         <Badge variant="secondary">5</Badge>
         <Badge variant="primary">2 de 5</Badge>
       </div>
+      <div>
+        <Button icon={PlusIcon}>Nova Tarefa</Button>
+      </div>
+      <div className="flex gap-1">
+        <ButtonIcon icon={TrashIcon} />
+        <ButtonIcon icon={TrashIcon} variant={"secondary"}/>
+        <ButtonIcon icon={TrashIcon} variant={"tertiary"}/>
+      </div>
+      <div>
+        <InputText />
+      </div>
+      <div>
+        <InputChecbox></InputChecbox>
+      </div>
+      <div>
+        <Card size="md">Olá undo</Card>
+      </div>
+      <div className="space-y-2">
+        <Skeleton className="h-6"></Skeleton>
+        <Skeleton className="h-6"></Skeleton>
+        <Skeleton className="w-96"></Skeleton>
+      </div>
     </div>
+    </Container>
   )
 }
