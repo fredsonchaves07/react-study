846044673da4f4b38bcbe8b68de155f1128a8a3d
Fredson Chaves <fredsonchaves07@gmail.com>
2025-07-02 12:23:22 -0300

    feat: implementando demais features de next e estilizações globais

--- 06-ignite-shop/src/pages/_app.tsx
+++ 06-ignite-shop/src/pages/_app.tsx
@@ -1,3 +1,19 @@
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { globalStyles } from "@/styles/global";
+import { AppProps } from "next/app";
+
+import logoImg from '@/assets/logo.svg'
+import { Container, Header } from "@/styles/pages/app";
+import Image from "next/image";
+
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg.src} alt="" width={logoImg.width} height={logoImg.height}/>
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
--- 06-ignite-shop/src/pages/_document.tsx
+++ 06-ignite-shop/src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import { getCssText } from "@/styles";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -8,6 +9,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@700&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+        <style id="stitches" dangerouslySetInnerHTML={{__html: getCssText()}}/>
       </Head> 
       <body>
         <Main />
