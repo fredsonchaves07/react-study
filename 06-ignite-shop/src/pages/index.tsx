8c444cbabc144d32de2bd725deac79486f22c2b1
Fredson Chaves <fredsonchaves07@gmail.com>
2025-07-29 12:20:39 -0300

    feat: ajustando preço dos produtos em formato real

--- 06-ignite-shop/src/pages/index.tsx
+++ 06-ignite-shop/src/pages/index.tsx
@@ -2,13 +2,9 @@ import Image from "next/image";
 import { HomeContainer, Product } from "./home";
 import { useKeenSlider } from 'keen-slider/react'
 
-import camiseta1 from '@/assets/camisetas/1.png'
-import camiseta2 from '@/assets/camisetas/2.png'
-import camiseta3 from '@/assets/camisetas/3.png'
-
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from "@/lib/stripe";
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, GetStaticProps } from "next";
 import Stripe from "stripe";
 
 interface HomeProps {
@@ -45,7 +41,7 @@ export default function Home({products}: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -55,13 +51,17 @@ export const getServerSideProps: GetServerSideProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount_decimal
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount != null ? price.unit_amount : 0 / 100)
     }
   })
 
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2
   }
 }
